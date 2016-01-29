a7c47cbd0d8ce2beeceb424afdeec8f6580b6f2e
Andrea Moretti <amoretti@ubuntu.corp.ad.zalando.net>
2016-01-29 12:32:44 +0100

    get rid of css

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const ENVIRONMENT = process.env.NODE_ENV || 'production';
 
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
 const conf = {
     devtool: 'source-maps',
     entry: './index',
@@ -13,16 +11,6 @@ const conf = {
     },
     module: {
         loaders: [
-            {
-                exclude: /node_modules/,
-                loader: ExtractTextPlugin.extract('style', 'css?sourceMap!postcss'),
-                test: /\.css$/
-            },
-            {
-                include: /node_modules/,
-                loader: 'style!css',
-                test: /\.css$/
-            },
             {
                 exclude: /node_modules/,
                 loader: 'babel',
@@ -41,14 +29,7 @@ const conf = {
         libraryTarget: 'umd',
         path: './dist/'
     },
-    plugins: [
-        new ExtractTextPlugin(
-            'react-zlide.css', {
-                allChunks: true
-            }
-        )
-    ],
-    postcss: require('./lib/postcss').processors
+    plugins: []
 };
 
 if (ENVIRONMENT === 'production') {
